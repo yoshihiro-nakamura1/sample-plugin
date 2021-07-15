7d21e171bcd0949fa81e43ca1bbf70d29400d5a1
yoshihiro-nakamura1 <76759481+yoshihiro-nakamura1@users.noreply.github.com>
2021-07-15 15:42:55 +0900

    初期設定です
    特に、ソースコードを書いていません。
    scaffoldの状態です。

---
+++ sample-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Sample Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     sample-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sample_Plugin
+ */
+
+// Your code starts here.
